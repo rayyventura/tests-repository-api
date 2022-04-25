ca32c868a7bce15cc578551684f48bad267ae0b4
Rayane Ventura <rayyventura@gmail.com>
2022-04-25 18:46:41 +0200

    deploy:adust to deploy

--- prisma/migrations/20220424231143_change_relation/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "tests" DROP CONSTRAINT "tests_teacherDisciplineId_fkey";
-
--- AddForeignKey
-ALTER TABLE "tests" ADD CONSTRAINT "tests_teacherDisciplineId_fkey" FOREIGN KEY ("teacherDisciplineId") REFERENCES "teachersdisciplines"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
